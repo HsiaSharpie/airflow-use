3c8c5fa6d6f87011df47c7eea32e2f96c348bbe6
HsiaSharpie <sam0213442dif@gmail.com>
2025-05-29 15:14:32 +0800

    test env

--- dags/kubernetesPodExecutor-test.py
+++ dags/kubernetesPodExecutor-test.py
@@ -2,23 +2,40 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
+from airflow.kubernetes.secret import Secret
 
+
+# 把 Kubernetes Secret 的內容掛成環境變數
+aws_access_key_id_secret = Secret(
+    deploy_type="env",              # 以環境變數形式掛載
+    deploy_target="AWS_ACCESS_KEY_ID",  # 這是 Pod 內看到的變數名
+    secret="aws-creds",             # Kubernetes Secret 名稱
+    key="AWS_ACCESS_KEY_ID",       # Secret 中的 key
+)
+
+aws_secret_access_key_secret = Secret(
+    deploy_type="env",
+    deploy_target="AWS_SECRET_ACCESS_KEY",
+    secret="aws-creds",
+    key="AWS_SECRET_ACCESS_KEY",
+)
 
 
 with DAG(
-    dag_id="simple_k8s_pod_operator",
+    dag_id="secrets_test",
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    hello_task = KubernetesPodOperator(
-        task_id="hello_k8s_pod",
-        name="hello-k8s-pod",
+    print_env = KubernetesPodOperator(
+        task_id="print-env",
+        name="print-env",
         namespace="airflow",
-        image="alpine:3.18",
+        image="python:3.9-slim",  # base image 就好
         cmds=["sh", "-c"],
-        arguments=["echo Hello from K8s Pod"],
+        arguments=["echo $AWS_ACCESS_KEY_ID && echo $AWS_SECRET_ACCESS_KEY"],
+        secrets=[aws_access_key_id_secret, aws_secret_access_key_secret],
         get_logs=True,
         is_delete_operator_pod=True,
     )
@@ -28,4 +45,4 @@ with DAG(
         bash_command="echo This runs after the K8s pod"
     )
 
-    hello_task >> bash_task
+    print_env >> bash_task
